e1dae66b7c52b73860c94d1bd3abe726db9780e7
bappi-repliq <abobashar.bappi@repliq.dev>
2023-10-07 16:23:52 +0600

    user model and setup settings

--- projectile/projectile/settings.py
+++ projectile/projectile/settings.py
@@ -10,11 +10,34 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# The root of the git repo - Could be ~/project or ~/repo
+REPO_DIR = os.path.realpath(os.path.join(BASE_DIR, ".."))
+# The directory of the current user ie /home/django a.k.a. ~
+HOME_DIR = os.path.realpath(os.path.join(REPO_DIR, ".."))
+# The directory where collectstatic command copies/symlinks the files to
+# This can/should be located at ~/staticfiles, preferrably outside the git repo
+STATIC_DIR = os.path.realpath(os.path.join(HOME_DIR, "staticfiles"))
+# The directory where different applications uploads media files to
+# This can/should be located at ~/media, preferrably outside the git repo
+MEDIA_DIR = os.path.realpath(os.path.join(HOME_DIR, "media"))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = STATIC_DIR
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
+
+
+STATICFILES_DIRS = [os.path.join(REPO_DIR, "assets")]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -30,14 +53,34 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
+PROJECT_APPS = [
+    "core",
+]
+
+THIRD_PARTY_APPS = [
+    "drf_yasg",
+]
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+# swagger settings
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {"basic": {"type": "basic"}},
+}
+
+REDOC_SETTINGS = {
+    "LAZY_RENDERING": False,
+}
+
+AUTH_USER_MODEL = "core.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +97,7 @@ ROOT_URLCONF = 'projectile.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,11 +154,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- projectile/projectile/urls.py
+++ projectile/projectile/urls.py
@@ -14,9 +14,48 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from rest_framework import permissions
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AiTools",
+        default_version="main",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=False,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Swagger
+    re_path(
+        r"^docs/swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=10),
+        name="schema-json",
+    ),
+    re_path(
+        r"^docs/swagger$",
+        schema_view.with_ui("swagger", cache_timeout=10),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^docs/redoc$",
+        schema_view.with_ui("redoc", cache_timeout=10),
+        name="schema-redoc",
+    ),
+    path("adminium/", admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
