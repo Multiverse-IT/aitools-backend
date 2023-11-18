34f4fa094667c1b66e38a967baf4fb859c4f02c1
bappi-repliq <abobashar.bappi@repliq.dev>
2023-11-18 17:07:11 +0600

    comment out 2

--- projectile/projectile/settings.py
+++ projectile/projectile/settings.py
@@ -192,7 +192,7 @@ REST_FRAMEWORK = {
 
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
-    "drf_social_oauth2.backends.DjangoOAuth2",
+    # "drf_social_oauth2.backends.DjangoOAuth2",
     # "social_core.backends.google.GoogleOAuth2",
 )
 
@@ -208,21 +208,21 @@ CORS_ALLOWED_ORIGINS = [
     "https://aitools-staging.vercel.app"
 ]
 
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-)
+# SOCIAL_AUTH_PIPELINE = (
+#     'social_core.pipeline.social_auth.social_details',
+#     'social_core.pipeline.social_auth.social_uid',
+#     'social_core.pipeline.social_auth.auth_allowed',
+#     'social_core.pipeline.social_auth.social_user',
+#     'social_core.pipeline.social_auth.associate_user',
+#     'social_core.pipeline.social_auth.load_extra_data',
+#     'social_core.pipeline.user.user_details',
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '654201802529-h3l30nfnnjr5oahptsjcq6j7oume7fbn.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '654201802529-h3l30nfnnjr5oahptsjcq6j7oume7fbn.apps.googleusercontent.com'
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 
 # SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
 #     "https://www.googleapis.com/auth/userinfo.email",
