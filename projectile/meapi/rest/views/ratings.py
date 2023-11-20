3eabb210565c03cfaa30e523d5d86909a57197d3
bappi-repliq <abobashar.bappi@repliq.dev>
2023-11-20 22:23:58 +0600

    me ratings

--- projectile/meapi/rest/views/ratings.py
+++ projectile/meapi/rest/views/ratings.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import generics
 
 from catalogio.models import Rating
@@ -6,6 +8,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from ..permissions import CustomIdentityHeaderPermission
 from ..serializers.ratings import MeRatingListDetaliSerializer
 
+User = get_user_model()
+
 class RatingList(generics.ListCreateAPIView):
     queryset = Rating.objects.filter()
     serializer_class = MeRatingListDetaliSerializer
@@ -13,6 +17,12 @@ class RatingList(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
+        identity = self.request.headers.get("identity")
+        user = User.objects.filter(id=identity).first()
+
+        if not user:
+            return Rating.objects.none()
+
         return self.queryset.filter(user=user)
 
 class RatingDetail(generics.RetrieveUpdateAPIView):
@@ -20,3 +30,10 @@ class RatingDetail(generics.RetrieveUpdateAPIView):
     serializer_class = MeRatingListDetaliSerializer
     permission_classes = [CustomIdentityHeaderPermission]
     lookup_field = "slug"
+
+    def get_queryset(self):
+        identity = self.request.headers.get("identity")
+        user = User.objects.filter(id=identity).first()
+        if not user:
+            return Rating.objects.none()
+        return Rating.objects.filter(user=user)
