acd0c96b7a4c64a04f0d0278334c6cad7b321caf
bappi-repliq <abobashar.bappi@repliq.dev>
2023-11-20 22:48:59 +0600

    permission class updated

--- projectile/meapi/rest/permissions.py
+++ projectile/meapi/rest/permissions.py
@@ -5,15 +5,12 @@ User = get_user_model()
 
 class CustomIdentityHeaderPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Get the identity from the headers
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
         identity = request.headers.get("identity")
-
-        # If identity is not present, deny permission
         if not identity:
             return False
 
-        # Check if the identity corresponds to a valid user
         user = User.objects.filter(id=identity).first()
-
-        # If user is found, grant permission
         return user is not None
