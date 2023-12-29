5be9f5f10ca50140f4f4b53a2aeae2bc02e6aea7
bappi-repliq <abobashar.bappi@repliq.dev>
2023-12-29 17:38:48 +0600

    related tools fixed

--- projectile/meapi/rest/serializers/tools.py
+++ projectile/meapi/rest/serializers/tools.py
@@ -325,7 +325,7 @@ class PublicTooDetailSerializer(serializers.ModelSerializer):
     def get_related_tools(self, instance):
         try:
             related_tools = (
-                Tool.objects.filter(status=ToolStatus.Active,
+                Tool.objects.get_status_active().filter(
                     toolscategoryconnector__category__id=instance.toolscategoryconnector_set.first().category.id
                 )
                 .exclude(id=instance.id)
