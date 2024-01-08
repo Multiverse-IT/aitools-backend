4b6bfb29debdbd3e5d12d164ee1e92f6f8fefe57
bappi-repliq <abobashar.bappi@repliq.dev>
2024-01-08 22:19:24 +0600

    storage update

--- projectile/weapi/rest/serializers/storage.py
+++ projectile/weapi/rest/serializers/storage.py
@@ -7,3 +7,13 @@ class PrivateCommonStorageSerializer(serializers.ModelSerializer):
         model = CommonStorage
         fields = ["uid", "storage", "created_at", "updated_at"]
         read_only_fields = ["uid", "created_at", "updated_at"]
+
+    def create(self, validated_data):
+        common_storage = CommonStorage.objects.first()
+        if common_storage:
+            storage = validated_data.get("storage")
+            common_storage.storage = storage
+            common_storage.save()
+        else:
+            common_storage = CommonStorage.objects.create(**validated_data)
+        return common_storage
