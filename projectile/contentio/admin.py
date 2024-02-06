52850a4358b614abbb22875d24a25588fd42b079
bappi-repliq <abobashar.bappi@repliq.dev>
2024-02-06 22:09:19 +0600

    redirect added to admin site

--- projectile/contentio/admin.py
+++ projectile/contentio/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post, CommonStorage
+from .models import Post, CommonStorage, Redirect
 
 admin.site.register(Post)
 
 admin.site.register(CommonStorage)
+
+admin.site.register(Redirect)
