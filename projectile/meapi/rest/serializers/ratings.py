7ba506c20e4f1e3ca33bbbd7b765ea659f9bc0ef
Bappi <abobashar.bappi@repliq.dev>
2024-04-06 13:23:55 +0600

    uid added to rating list

--- projectile/meapi/rest/serializers/ratings.py
+++ projectile/meapi/rest/serializers/ratings.py
@@ -17,6 +17,7 @@ class MeRatingListDetaliSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = [
+            "uid",
             "pros",
             "cons",
             "review",
