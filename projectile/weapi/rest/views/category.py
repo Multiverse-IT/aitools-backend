38672ac7bd4b3396409d33cc5787ac5a624a8f7d
abbappii <bappi142434@gmail.com>
2024-10-29 00:39:42 +0600

    added

--- projectile/weapi/rest/views/category.py
+++ projectile/weapi/rest/views/category.py
@@ -27,7 +27,7 @@ class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class SubCategoryList(generics.ListCreateAPIView):
     serializer_class = SubCatetoryListDetailSerializer
-    permission_classes = [IsAdmin]
+    permission_classes = []
 
     def get_queryset(self):
         return SubCategory.objects.filter()
@@ -37,11 +37,11 @@ class SubcategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = SubCategory.objects.filter()
     serializer_class = SubCatetoryListDetailSerializer
     lookup_field = "slug"
-    permission_classes = [IsAdmin]
+    permission_classes = []
 
 
 class SubCategoryListWithCategoryTitle(APIView):
-    permission_classes = [IsAdmin]
+    permission_classes = []
 
     def get(self, request):
         subcategory_dict = {}
